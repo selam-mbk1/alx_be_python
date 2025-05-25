23f32b22a86687b29ed4d75202a57fc8b6a5af5a
selam-mbk1 <selamawitbasaznew@gmail.com>
2025-05-25 18:23:11 +0300

    Create simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000      # The initial amount invested
+rate = 0.05           # Annual interest rate (5%)
+time = 3              # Duration in years
+
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
