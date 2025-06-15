820c8e16b69c0469b5b6803dad5911aa124cda39
selam-mbk1 <selamawitbasaznew@gmail.com>
2025-06-15 21:26:45 +0300

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,27 +1,3 @@
-# simple_calculator.py
-
-class SimpleCalculator:
-    """A simple calculator class that supports basic arithmetic operations."""
-
-    def add(self, a, b):
-        """Return the addition of a and b."""
-        return a + b
-
-    def subtract(self, a, b):
-        """Return the subtraction of b from a."""
-        return a - b
-
-    def multiply(self, a, b):
-        """Return the multiplication of a and b."""
-        return a * b
-
-    def divide(self, a, b):
-        """Return the division of a by b. Returns None if b is zero."""
-        if b == 0:
-            return None
-        return a / b
-# test_simple_calculator.py
-
 import unittest
 from simple_calculator import SimpleCalculator
 
@@ -31,29 +7,29 @@ class TestSimpleCalculator(unittest.TestCase):
         """Set up the SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
-        """Test addition of different numbers."""
+    def test_addition(self):
+        """Test the addition method."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 5), 4)
         self.assertEqual(self.calc.add(0, 0), 0)
         self.assertEqual(self.calc.add(-4, -6), -10)
 
     def test_subtract(self):
-        """Test subtraction of different numbers."""
+        """Test subtraction method."""
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(3, 5), -2)
         self.assertEqual(self.calc.subtract(0, 0), 0)
         self.assertEqual(self.calc.subtract(-3, -7), 4)
 
     def test_multiply(self):
-        """Test multiplication of different numbers."""
+        """Test multiplication method."""
         self.assertEqual(self.calc.multiply(4, 3), 12)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
         self.assertEqual(self.calc.multiply(0, 100), 0)
         self.assertEqual(self.calc.multiply(-4, -2), 8)
 
     def test_divide(self):
-        """Test division of different numbers."""
+        """Test division method."""
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(-10, 2), -5)
         self.assertEqual(self.calc.divide(10, -2), -5)
